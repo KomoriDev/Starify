e8dcdf58ab96d6a83b26f7501c0ec08f7dd1d1af
KomoriDev <mute231010@gmail.com>
2025-02-22 21:32:02 +0800

    :adhesive_bandage: add cache-control headers

--- src/pages/api/starify.tsx
+++ src/pages/api/starify.tsx
@@ -118,6 +118,9 @@ export default async function handler(req: NextRequest) {
     {
       width: totalWidth,
       height: 96,
+      headers: {
+        'Cache-Control': 'max-age=0, s-maxage=60, stale-while-revalidate',
+      }
     }
   )
 }
